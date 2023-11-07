204d717088304f0e4ee7253d4d7271cbe1faff93
kait-takei <s2121040@cco.kanagawa-it.ac.jp>
2023-11-07 20:59:02 +0900

    マップ表示、カメラ起動

---
+++ lib/src/Map/Capsel/capsel_Check.dart
@@ -0,0 +1,41 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_develop/src/Map/Capsel/capsel_Create.dart';
+import 'package:flutter_application_develop/src/app.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Sample2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('カプセル確認画面'),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      body: Column(//縦並び
+          children: <Widget>[
+        OutlinedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                //マップ画面に遷移(Navigatorpopの方がいいかも？)
+                return capsel_Create();
+              }),
+            );
+          },
+          child: Text('戻る'),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            //カプセルを埋める画面に遷移
+          },
+          child: Text('埋める！'),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/src/Map/Capsel/capsel_Create.dart
@@ -0,0 +1,238 @@
+import 'dart:ffi';
+//import 'dart:html';
+import 'package:flutter_application_develop/src/Map/Capsel/capsel_Check.dart';
+import 'package:flutter_application_develop/src/Map/Capsel/pucture_View.dart';
+import 'package:flutter_application_develop/src/Map/Map.dart';
+import 'package:flutter_application_develop/src/app.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_map/flutter_map.dart';
+//import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+
+class capsel_Create extends StatelessWidget {
+  const capsel_Create({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  /*image_pickerを利用するためのコード
+  File _image;
+  final picker = ImagePicker();*/
+
+  String _apptitle = 'カプセル埋める';
+  String title = '';
+  String nakami = '';
+
+  /*カメラ画面を表示する
+  Future Camera_Show() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('imageが選択されていません');
+      }
+    });
+  }*/
+
+  //追記：位置情報取得
+  String _location = "";
+  Future<void> getLocation() async {
+    // 現在の位置を返す
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print("緯度: " + position.latitude.toString());
+    // 東経がプラス、西経がマイナス
+    print("経度: " + position.longitude.toString());
+
+    setState(() {
+      _location = position.toString();
+    });
+  }
+  //追記部終わり
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  dynamic dateTime;
+
+  @override
+  void initState() {
+    super.initState();
+    dateTime = DateTime.now();
+  }
+
+//DatePicker設定画面
+  _datePicker(BuildContext context) async {
+    final DateTime? datePicked = await showDatePicker(
+        context: context,
+        initialDate: dateTime,
+        firstDate: DateTime(2003),
+        lastDate: DateTime(2100));
+    if (datePicked != null && datePicked != dateTime) {
+      setState(() {
+        dateTime = datePicked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //カメラ表示追記
+    final ImagePicker _picker = ImagePicker();
+    return Scaffold(
+      //キーボードを出した時に、bottom～のトラテープみたいなエラーを封じる
+      resizeToAvoidBottomInset: false,
+      // appBar: AppBar(
+      // ),
+
+      //columnで画面範囲を超えてbottom～エラーが出た時に封じる↓
+      //画面を下に引っ張って更新することは不可?
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 40, left: 20, right: 20), //画面全体の余白
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start, // 垂直方向上寄せ
+            children: [
+              Container(
+                  child: ButtonBar(
+                      //横並びにする
+                      alignment: MainAxisAlignment.spaceBetween, //幅を等しくする
+                      children: [
+                    //キャンセルボタンを押した時の処理
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            //マップ画面に遷移(Navigatorpopの方がいいかも？)
+                            return MapScreen();
+                          }),
+                        );
+                      },
+                      child: const Text("キャンセル"),
+                    ),
+
+                    //次へボタンを押した時の処理
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            //マップ画面に遷移(Navigatorpopの方がいいかも？)
+                            return Sample2();
+                          }),
+                        );
+                      },
+                      child: const Text("次へ"),
+                    ),
+                  ])),
+
+              //撮影するボタン
+              Container(
+                child: Column(children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      //カメラ撮影画面に遷移
+                      final XFile? image = await _picker.pickImage(
+                        source: ImageSource.camera,
+                      );
+                      if (image != null)
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => done(image),
+                          ),
+                        );
+                    },
+                    child: const Text("撮影する！"),
+                  ),
+                ]),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9, // 画面幅の90%に設定
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'タイトル',
+                  ),
+                  onChanged: (text) {
+                    // TODO: ここで取得したtextを使う
+                    title = text;
+                  },
+                ),
+              ),
+              SizedBox(height: 20.0), // 適切な間隔を設定
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9, // 画面幅の90%に設定
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: '中身',
+                  ),
+                  onChanged: (text) {
+                    // TODO: ここで取得したtextを使う
+                    nakami = text;
+                  },
+                ),
+              ),
+
+              Text("$dateTime", style: TextStyle(fontSize: 25)),
+
+              ElevatedButton(
+                onPressed: () {
+                  _datePicker(
+                    context,
+                  );
+                },
+                child: const Text("日付を変更"),
+              ),
+
+              //位置情報取得ボタン
+              ElevatedButton(
+                onPressed: () {
+                  getLocation();
+                },
+                child: const Text("位置情報取得"),
+              ),
+              //仮
+              Image.network(
+                'https://pbs.twimg.com/media/FfHOaRIagAAxQlC.jpg',
+                width: 50,
+                height: 100,
+              ),
+
+              //位置情報テキスト
+              Text('$_location'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
