8dccdc944eeb8ba9f56b6176417069d93018c63a
MikamiTakuhiro <b1021082@fun.ac.jp>
2023-11-16 00:11:45 +0900

    map自分の位置表示

--- lib/src/Map/Map.dart
+++ lib/src/Map/Map.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_develop/src/Map/Capsel/capsel_Create.dart';
-import 'package:flutter_application_develop/src/app.dart';
-//追加import
-import 'dart:ffi';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import '../theme_setting/Color_Scheme.dart';
+import 'package:flutter_application_develop/src/Map/Capsel/capsel_Create.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:geocoding/geocoding.dart' as geoCoding;
 
 final ThemeData lightTheme =
     ThemeData(useMaterial3: true, colorScheme: lightColorScheme);
@@ -31,66 +29,39 @@ class MapScreen extends StatelessWidget {
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
+
   final String title;
+
   @override
   State<HomeScreen> createState() => _HomeScreen();
 }
 
 class _HomeScreen extends State<HomeScreen> {
-//マップを表示する
-  String _title = 'MyMap';
-  double lat = 35.995; //メモ：最初に表示される場所を指定
-  double lng = 138.158;
-  var currentPosition; //メモ：現在地を取得する変数
-  var currentHeading;
+  //String _title = 'MyMap';
+  double? lat; //メモ：最初に表示される場所を指定
+  double? lng;
   LatLng? tapLatLng; //メモ：?はnullも許容
-
   List<Marker> markers = [];
-
-  List<CircleMarker> circleMarkers = [
-    //中身ないけど、削除すると現在地の●表示されなくなる
-  ];
+  List<CircleMarker> circleMarkers = []; //中身ないけど、削除すると現在地の●表示されなくなる
+  bool isLoading = true; // 追加: ローディング状態を管理
 
   @override
   void initState() {
     super.initState();
-    /*var future = initLocation();
-    future.then((newCorrentPosition) => print("get ${newCorrentPosition}"));*/
-    Future<Position> newCorrentPosition = initLocation(); //現在地
-    print('returnしたいもの${newCorrentPosition}');
-  }
-
-  //この関数の戻り値でlatitudeとlongtitudeの値(端末の現在地)
-  Future<Position> initLocation() async {
-    //メモ：多分位置を取得するのを許可している？
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-    //メモ：Geo~.get~で現在地の位置情報取得し、currentPositionに代入
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    currentHeading = position.heading;
-    currentPosition = position;
-    double latitude = position.latitude; //メモ：現在地読み込み？
-    double longtitude = position.longitude;
-    initCircleMarker(latitude, longtitude);
-    lat = latitude;
-    lng = longtitude;
-    if (mounted) {
-      setState(() {
-        print('currentPositionは${Geolocator.getCurrentPosition()}で端末の場所');
-      });
-    }
-    return currentPosition;
+    initLocation();
   }
 
   @override
   Widget build(BuildContext context) {
     ThemeData selectedTheme = lightTheme;
+    // 追加: isLoadingがtrueの場合はローディング表示
+    if (isLoading) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return MaterialApp(
       theme: selectedTheme,
       home: Scaffold(
@@ -108,17 +79,18 @@ class _HomeScreen extends State<HomeScreen> {
           child: FlutterMap(
             // マップ表示設定
             options: MapOptions(
-                center: LatLng(lat, lng), //メモ:ここを変える
-                zoom: 10.0,
-                interactiveFlags: InteractiveFlag.all,
-                enableScrollWheel: true,
-                scrollWheelVelocity: 0.00001,
-                onTap: (tapPosition, latLng) {
-                  tapLatLng = latLng;
-                  print(tapLatLng);
-                  print("↑ピンを押したところの緯度経度");
-                  //cleatePin(tapLatLng!);
-                }),
+              center: LatLng(lat!, lng!), //メモ:ここを変える
+              zoom: 15.0,
+              interactiveFlags: InteractiveFlag.all,
+              enableScrollWheel: true,
+              scrollWheelVelocity: 0.00001,
+              onTap: (tapPosition, latLng) {
+                tapLatLng = latLng;
+                print(tapLatLng);
+                print("↑ピンを押したところの緯度経度");
+                cleatePin(tapLatLng!);
+              },
+            ),
             layers: [
               //背景地図読み込み (OSM)
               TileLayerOptions(
@@ -151,21 +123,9 @@ class _HomeScreen extends State<HomeScreen> {
                 //カプセル作成画面に遷移
                 return capsel_Create();
               }),
-            );
+            ); // 追加ボタンが押されたときの処理を追加
           },
         ),
-        /*Column(
-              children: [
-                const Center(
-                    child: Text('Map画面', style: TextStyle(fontSize: 32.0))),
-                 Switch(
-                   value: _light,
-                   onChanged: (bool value) {
-                     _toggleTheme();
-                   },
-                 ),
-              ],
-            )*/
       ),
     );
   }
@@ -195,7 +155,7 @@ class _HomeScreen extends State<HomeScreen> {
     }
   }
 
-  Future<void> onTapPinCallBack(LatLng latLng) async {
+  void onTapPinCallBack(LatLng latLng) {
     final tapMarker = pinReturn(latLng, pinReturn);
     markers.add(tapMarker);
     markers.removeAt(1);
@@ -225,7 +185,38 @@ class _HomeScreen extends State<HomeScreen> {
     );
   }
 
-  //メモ：端末の現在地を青色の●で表示する関数
+  //この関数の戻り値でlatitudeとlongtitudeの値(端末の現在地)
+  Future<void> initLocation() async {
+    //メモ：多分位置を取得するのを許可している？
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+//メモ：Geo~.get~で現在地の位置情報取得し、currentPositionに代入
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // double latitude = position.latitude; //メモ：現在地読み込み？
+    // double longitude = position.longitude;
+    // //取得した緯度経度からその地点の地名情報を取得する
+    // final placeMarks = await geoCoding.placemarkFromCoordinates(lat!, lng!);
+    // final placeMark = placeMarks[0];
+    // print("現在地の国は、${placeMark.country}");
+    // print("現在地の県は、${placeMark.administrativeArea}");
+    // print("現在地の市は、${placeMark.locality}");
+    setState(() {
+      lat = position.latitude;
+      lng = position.longitude;
+      isLoading = false; // 追加: ローディング終了
+    });
+
+    initCircleMarker(lat!, lng!);
+  }
+
+//メモ：端末の現在地を青色の●で表示する関数
   void initCircleMarker(double latitude, double longitude) {
     CircleMarker circleMarker = CircleMarker(
       color: Colors.indigo.withOpacity(0.9),
