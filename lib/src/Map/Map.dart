bbb21ad32f1ba55edab34f3dcd302fd38d70f0f2
kait-takei <s2121040@cco.kanagawa-it.ac.jp>
2023-11-22 22:37:03 +0900

    サーバ送信未完成

--- lib/src/Map/Map.dart
+++ lib/src/Map/Map.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_develop/src/Map/Capsel/picture_Check.dart';
+import 'package:flutter_application_develop/src/Map/Capsel/picture.dart';
+import 'package:flutter_application_develop/src/Map/Capsel/picture.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
