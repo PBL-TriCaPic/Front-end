f2500b928f9a0d64edf0edc89dba11108db7126f
kait-takei <s2121040@cco.kanagawa-it.ac.jp>
2023-11-22 22:40:32 +0900

    未完成

--- lib/src/Map/Capsel/picture_Check.dart
+++ lib/src/Map/Capsel/picture_Check.dart
@@ -12,40 +12,10 @@ import 'package:image_picker/image_picker.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class PictureCheck extends StatelessWidget {
-  PictureCheck(this.image, {Key? key}) : super(key: key);
+class done extends StatelessWidget {
+  const done(this.image, {Key? key}) : super(key: key);
   final XFile image;
-  late SharedPreferences pref;
-  File? imageFile;
-
-/*class MyHomePage extends StatefulWidget {
-  final File image;
-  const MyHomePage({super.key, required this.title, required this.image});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => MyHomePageState();
-}*/
-
-/*@override
-  void initState() {
-    initState();
-    loadPref();
-  }
-
-  Future<void> loadPref() async {
-    late SharedPreferences pref;
-    pref = await SharedPreferences.getInstance();
-    setState(() {
-      image_pref = pref.getString('title')!;
-    });
-  }*/
-//class MyHomePageState extends State<MyHomePage> {
-
-  //late XFile image;
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +24,6 @@ class PictureCheck extends StatelessWidget {
       body: Column(children: [
         Center(
           child: Image.file(
-            //撮影した画像を表示
             File(image.path),
           ),
         ),
@@ -71,7 +40,7 @@ class PictureCheck extends StatelessWidget {
               //サーバのURLを指定する
               Uri url = Uri.parse('');
               String body = json.encode({
-                'image.path': base64Image,
+                '撮影した画像.jpg?': base64Image,
               });
               /*画像をデコードして表示　ここに書くとimage.readAsBytes()がエラー
               Response response = await http.post(url, body: body);
@@ -95,13 +64,6 @@ class PictureCheck extends StatelessWidget {
         ElevatedButton(
           child: Text('次へ進む'),
           onPressed: () async {
-            await getSharedPreference();
-            if (image != null) {
-              await pref.setString('imagePath', image!.path);
-              //setState(() {});
-            }
-            //await saveImagePath();
-            //setState();
             Navigator.push(
               context,
               MaterialPageRoute(
@@ -123,27 +85,6 @@ class PictureCheck extends StatelessWidget {
         ),
       ]),
     );
-  }
-
-  Future<void> saveImagePath() async {
-    getSharedPreference();
-    if (image != null) {
-      await pref.setString('imagePath', image!.path);
-      //setState(() {});
-    }
-  }
-
-  Future<void> getSharedPreference() async {
-    pref = await SharedPreferences.getInstance();
-  }
-
-  Future<void> setImage() async {
-    await getSharedPreference();
-    final String? imagePath = pref.getString('imagePath');
-    if (imagePath != null) {
-      imageFile = File(imagePath);
-      //setState(() {});
-    }
   }
 }
 //kotlinのverを1.7.0に変更(カメラ起動に関係する？)
