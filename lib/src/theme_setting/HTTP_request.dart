a91357e2dd6435016b2a377e0077e09dd4be2b89
toshima <114457585+toshima-k@users.noreply.github.com>
2024-01-10 23:57:48 +0900

    スクショ不可

--- lib/src/theme_setting/HTTP_request.dart
+++ lib/src/theme_setting/HTTP_request.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseApiUrl = 'http://192.168.10.119:8081/api';
+  static const String baseApiUrl = 'http://10.0.2.2:8081/api';
 
   static Future<Map<String, dynamic>> loginUser(
       String email, String password) async {
